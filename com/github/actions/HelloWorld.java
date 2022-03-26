7b6d226732d64ed680640523c99bf47e058381f5
naveen-rd <naveen.flash@gmail.com>
2022-03-26 00:49:28 -0500

    Update HelloWorld.java

--- com/github/actions/HelloWorld.java
+++ com/github/actions/HelloWorld.java
@@ -1,29 +1,23 @@
 package com.github.actions;
 
-/**
- * Hello world.
- *
- **/
-public class HelloWorld { 
-    
-    HelloWorld() {
-        System.out.println("Default constructor");
-    }
-    
-    /**
-     * main method.
-     * @param args
-     **/
-    public static void main(final String[] args) {
-        HelloWorld hw = new HelloWorld();
-        hw.myMethod();
-        System.out.println("Hello World!! Foo");
-    }
+/** Hello world. */
+public class HelloWorld {
 
-    /**
-     * Test method.
-     **/
-    public void myMethod() {
-        System.out.println("Void method of the class");
-    }
+  HelloWorld() {
+    System.out.println("Default constructor");
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(final String[] args) {
+    HelloWorld hw = new HelloWorld();
+    hw.myMethod();
+    System.out.println("Hello World!! Foo");
+  }
+
+  /** Test method. */
+  public void myMethod() {
+    System.out.println("Void method of the class");
+  }
 }
